4d3dfb492849318628bd6c19c340533f1dc126dd
saleh afzoon <saleh.afzoon.76@gmail.com>
2020-02-27 18:54:34 +0330

    rewire code with heep memory allocation

--- lup_matrix_inverse.c
+++ lup_matrix_inverse.c
@@ -2,10 +2,10 @@
     Homepage: https://sites.google.com/site/chandraacads
  * * */
 
-
 #include <stdio.h>
 #include <float.h>
 #include <math.h>
+#include <stdlib.h>
 
 /* The following header file contains a 128 bit float data type '__float128'*/
 //# include <quadmath.h>
@@ -13,14 +13,13 @@
 typedef float Type;
 
 #define N 1000 //Size of square matrix 'A'.
-#define MAX N+1
+#define MAX N + 1
 
-static int LUPdecompose(int size, Type A[MAX][MAX], int P[MAX]);
+static int LUPdecompose(int size, Type **A, int *P);
 
-static int LUPinverse(int size, int P[MAX], Type LU[MAX][MAX],
-                      Type B[MAX][MAX], Type X[MAX], Type Y[MAX]);
+static int LUPinverse(int size, int *P, Type **LU, Type **B, Type *X, Type *Y);
 
-void permutation_print(int P[N + 1])
+void permutation_print(int *P)
 {
   printf("\n\nThe LUP decomposition of 'A' is successful.\nPivot:\n");
   for (int i = 1; i <= N; i++)
@@ -32,35 +31,43 @@ void permutation_print(int P[N + 1])
 }
 void print_system_data_type_info()
 {
-  
+
   printf("On this machine, size (in bytes) and precision (in number of decimal digits)"
          " of\n\tfloat: %ld and %d,\n\tdouble: %d and %d,",
          sizeof(float), FLT_DIG,
          sizeof(double), DBL_DIG);
 
-  printf("\n\nmatrix size is : %d",N);
-
+  printf("\n\nmatrix size is : %d", N);
 }
 
-void initial_matix(Type A[N + 1][N + 1], Type A1[N + 1][N + 1])
+void initial_matix(Type **A, Type **A1)
 {
+
   for (int i = 1; i <= N; i++)
     for (int j = 1; j <= N; j++)
       A[i][j] = A1[i][j] = sin(i * j * j + i) * 2;
+
+  if (A == NULL || A1 == NULL)
+  {
+    printf("Memory not allocated.\n");
+    exit(0);
+  }
+  printf("initial matrix done");
 }
 
-void print_matrix(Type A[N + 1][N + 1])
+void print_matrix(Type **A)
 {
   printf("\n\nThe to-be-inverted matrix 'A':\n");
+
   for (int i = 1; i <= N; i++)
   {
     for (int j = 1; j <= N; j++)
-      printf("\t%E", (float)A[i][j]);
+      printf("\t%E", (Type)A[i][j]);
     printf("\n");
   }
 }
 
-void print_LU_matrix(Type A[N + 1][N + 1])
+void print_LU_matrix(Type **A)
 {
 
   printf("\nLU (where 1. the diagonal of the matrix belongs to 'U', and 2. the\n"
@@ -73,7 +80,7 @@ void print_LU_matrix(Type A[N + 1][N + 1])
   }
 }
 
-void print_inverting_result(Type A[N + 1][N + 1])
+void print_inverting_result(Type **A)
 {
   printf("\n\nMatrix inversion successful.\nInverse of A:\n");
   for (int j = 1; j <= N; j++)
@@ -83,8 +90,7 @@ void print_inverting_result(Type A[N + 1][N + 1])
     printf("\n");
   }
 }
-void check_inverse_correction(Type A[N + 1][N + 1], Type A1[N + 1][N + 1],
-                              Type I[N + 1][N + 1])
+void check_inverse_correction(Type **A, Type **A1, Type **I)
 {
   int i, j, k;
 
@@ -101,25 +107,37 @@ void check_inverse_correction(Type A[N + 1][N + 1], Type A1[N + 1][N + 1],
     printf("\n");
   }
   printf("\n");
+}
 
+Type **allocate_2d(Type **pointer)
+{
+  pointer = (Type **)malloc((N + 1) * sizeof(Type *));
+  for (int i = 0; i < N + 1; i++)
+    pointer[i] = (Type *)malloc((N + 1) * sizeof(Type));
+
+  return pointer;
 }
 
 int main()
 {
-  
-  Type A[N + 1][N + 1], A1[N + 1][N + 1];
+
+  // Type A[N + 1][N + 1], A1[N + 1][N + 1];
+  Type **A, **A1, **I;
+  A = allocate_2d(A);
+  A1 = allocate_2d(A1);
+  I = allocate_2d(I);
 
   //permutation matrix
-  int P[N + 1];
+  // int P[N + 1];
+  int *P = (int *)malloc((N + 1) * sizeof(int));
 
-  Type B[N + 1][N + 1], X[N + 1], Y[N + 1]; //Temporaty spaces.
+  // Type B[N + 1][N + 1], X[N + 1], Y[N + 1]; //Temporaty spaces.
+  Type **B, *X, *Y; //Temporaty spaces.
+  B = allocate_2d(B);
+  X = (Type *)malloc((N + 1) * sizeof(Type));
+  Y = (Type *)malloc((N + 1) * sizeof(Type));
 
-  print_system_data_type_info();
-
-// #ifdef QUADMATH_H
-//   printf("\n\t__float128: %ld and %d,", sizeof(__float128), FLT128_DIG);
-// #endif
-//   printf(" respectively.");
+  // print_system_data_type_info();
 
   /* Defining the to-be-inverted matrix, A. A1 would be used later to test the inverted
  * matrix. */
@@ -129,7 +147,8 @@ int main()
   // print_matrix(A);
 
   /* Performing LUP-decomposition of the matrix 'A'*/
-  if (LUPdecompose(N + 1, A, P) < 0){
+  if (LUPdecompose(N + 1, A, P) < 0)
+  {
     printf("\nLUPdecompose faild");
     return -1;
   }
@@ -139,7 +158,8 @@ int main()
   // print_LU_matrix(A);
 
   /* Inverting based on the LUP ,The inverse is stored in 'A' itself. */
-  if (LUPinverse(N + 1, P, A, B, X, Y) < 0){
+  if (LUPinverse(N + 1, P, A, B, X, Y) < 0)
+  {
     printf("\nLUPinverse faild");
     return -1;
   }
@@ -148,16 +168,22 @@ int main()
 
   /* check correctness of result by calculate A.A1 ,result must be I */
 
-  // check_inverse_correction(A,A1,I);
+  // check_inverse_correction(A, A1, I);
 
   printf("\n\nsuccessfully finished\n");
+
+  free(A);
+  free(A1);
+  free(B);
+  free(X);
+  free(Y);
 
   return 0;
 }
 
 /* This function decomposes the matrix 'A' into L, U, and P. If successful,
  * the L and the U are stored in 'A', and information about the pivot in 'P'. */
-static int LUPdecompose(int size, Type A[MAX][MAX], int P[MAX])
+static int LUPdecompose(int size, Type **A, int *P)
 {
   int i, j, k, kd = 0, T;
   Type p, t;
@@ -215,8 +241,7 @@ static int LUPdecompose(int size, Type A[MAX][MAX], int P[MAX])
 /* inverse of the LUP decomposed matrix 'LU' and pivot 'P'.
  * The inverse is returned as 'LU'.
  * 'B', X', and 'Y' are used as temporary spaces. */
-static int LUPinverse(int size, int P[MAX], Type LU[MAX][MAX],
-                      Type B[MAX][MAX], Type X[MAX], Type Y[MAX])
+static int LUPinverse(int size, int *P, Type **LU, Type **B, Type *X, Type *Y)
 {
   int i, j, n, m;
   Type t;
