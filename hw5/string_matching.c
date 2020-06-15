eae5de0c31e0283016b34e63d2ea3ebe63ec392d
saleh afzoon <saleh.afzoon.76@gmail.com>
2020-06-16 01:51:10 +0430

    mutation function added

--- hw5/string_matching.c
+++ hw5/string_matching.c
@@ -2,7 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define population_size 4
+#define population_size 10
+
+static const char alphanum[] =
+        " "
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // "abcdefghijklmnopqrstuvwxyz";
+
 struct Individual
 {
     char* chromosome;
@@ -12,15 +18,17 @@ struct Individual
     void (*evaluate)(struct Individual*);
     void (*mutate)(struct Individual*);
     void (*print)(struct Individual*);
-}Individual;
+};
 
 void evaluation(struct Individual* ind)
 {
     ind->fitness = 10;
 }
-void mutation(struct Individual* ind)
-{
-    ind->fitness = 10;
+void mutation(struct Individual* ind){
+
+    int index = rand()%strlen(ind->chromosome);
+
+    ind->chromosome[index] = alphanum[rand() % (sizeof(alphanum) - 1)];
 }
 void print_str(struct Individual* ind)
 {
@@ -31,10 +39,6 @@ void creation(struct Individual* ind,int size) {
     ind->chromosome = (char*)malloc((size+1)*sizeof(char));
     strncpy(ind->chromosome, "", size+1);
     
-    static const char alphanum[] =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "abcdefghijklmnopqrstuvwxyz";
-
     for (int i = 0; i < size; ++i) {
         ind->chromosome[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
     }
@@ -57,45 +61,49 @@ struct Individual** initialize_population(int size){
     return population;
 }
 
-
+/* qsort fitness comparison function */ 
 int sorter(const void *p1, const void *p2) {
 
+    const struct Individual *in1 = (struct Individual *)p1;
+    const struct Individual *in2 = (struct Individual *)p2;
     
-    // const struct Individual *in1 = (struct Individual *)p1;
-    // const struct Individual *in2 = (struct Individual *)p2;
-    
-    // if (in1->fitness < in2->fitness)
-    //     return -1;
-    // else if (in1->fitness > in2->fitness)
-    //     return +1;
-    // else
-    //     return 0;
-    return -1;
+    if (in1->fitness < in2->fitness)
+        return -1;
+    else if (in1->fitness > in2->fitness)
+        return +1;
+    else
+        return 0;
 }
- 
 
 int main(void)
 {
 
     int i=0;
-    char target[20] = "saleh";
+    char target[30] = "HELLO WORLD";
     int str_size = strlen(target);
     struct Individual** population;
     
     population = initialize_population(str_size);
-    
+
+        
     for (i = 0; i < population_size; i++)
     {
      population[i]->print(population[i]);   
     }
-    printf("----------------");
+    // printf("----------------\n");
 
-    qsort(population, population_size, sizeof(struct Individual), sorter);
+    // printf("%d\n",sorter( ((const void*)population[0]),((const void*)population[1])));
 
-    for (i = 0; i < population_size; i++)
-    {
-     population[i]->print(population[i]);   
-    }
+    // population[0]->print(population[0]);   
+    // population[0]->mutate(population[0]);
+    // population[0]->print(population[0]);
+        
+    // qsort(population, population_size, sizeof(struct Individual), sorter);
+
+    // for (i = 0; i < population_size; i++)
+    // {
+    //     population[i]->print(population[i]);   
+    // }
 
     return 0;
 }
